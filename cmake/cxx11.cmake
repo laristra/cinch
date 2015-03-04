498a0423fea7311b17b4e4fe19b49420b347b2d1
Ben Bergen <bergen@lanl.gov>
2015-03-04 12:24:53 -0700

    Moved useful macro into cmake directory.

---
+++ cmake/cxx11.cmake
@@ -0,0 +1,40 @@
+#~----------------------------------------------------------------------------~#
+# Copyright (c) 2014 Los Alamos National Security, LLC
+# All rights reserved.
+#~----------------------------------------------------------------------------~#
+
+# Determines whether or not the compiler supports C++11
+macro(check_for_cxx11_compiler _VAR)
+
+    message(STATUS "Checking for C++11 compiler")
+
+    set(${_VAR})
+
+    if((MSVC AND (MSVC10 OR MSVC11 OR MSVC12)) OR
+        (CMAKE_COMPILER_IS_GNUCXX AND
+            NOT ${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS 4.6) OR
+        (CMAKE_CXX_COMPILER_ID STREQUAL "Intel" AND
+            NOT ${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS 12.10) OR
+        (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND
+            NOT ${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS 3.1))
+
+        set(${_VAR} 1)
+        message(STATUS "Checking for C++11 compiler - available")
+
+    else()
+
+        message(STATUS "Checking for C++11 compiler - unavailable")
+
+    endif()
+
+endmacro()
+
+# Sets the appropriate flag to enable C++11 support
+macro(enable_cxx11)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+endmacro()
+
+#~---------------------------------------------------------------------------~-#
+# Formatting options for emacs and vim.
+# vim: set tabstop=4 shiftwidth=4 expandtab :
+#~---------------------------------------------------------------------------~-#
