cff99b119f66ba5d018a06c6b1fcb80aec251f89
Ben Bergen <bergen@lanl.gov>
2015-07-09 15:51:27 -0600

    Adding support for multiple libraries

---
+++ cmake/library.cmake
@@ -0,0 +1,27 @@
+#------------------------------------------------------------------------------#
+# Copyright (c) 2014 Los Alamos National Security, LLC
+# All rights reserved.
+#------------------------------------------------------------------------------#
+
+#
+# cinch_add_library
+#
+
+function(cinch_add_library_target target directory)
+
+    #--------------------------------------------------------------------------#
+    #
+    #--------------------------------------------------------------------------#
+
+    message(STATUS
+        "Adding library target ${target} with source directory ${directory}")
+
+    list(APPEND CINCH_LIBRARY_TARGETS "${target}:${directory}")
+    set(CINCH_LIBRARY_TARGETS ${CINCH_LIBRARY_TARGETS} PARENT_SCOPE)
+
+endfunction(cinch_add_library_target)
+
+#------------------------------------------------------------------------------#
+# Formatting options for emacs and vim.
+# vim: set tabstop=4 shiftwidth=4 expandtab :
+#------------------------------------------------------------------------------#
