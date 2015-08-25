e1cc058bbf936de4b7319bbada72d26c101969e1
Ben Bergen <ben.bergen@gmail.com>
2015-08-24 20:39:58 -0600

    Added C99 compiler check

---
+++ cmake/c99.cmake
@@ -0,0 +1,44 @@
+#~----------------------------------------------------------------------------~#
+# Copyright (c) 2014 Los Alamos National Security, LLC
+# All rights reserved.
+#~----------------------------------------------------------------------------~#
+
+# Determines whether or not the compiler supports C99
+macro(check_for_c99_compiler _VAR)
+
+    message(STATUS "Checking for C99 compiler")
+
+    set(${_VAR})
+
+    if((MSVC AND MSVC12) OR
+        (CMAKE_COMPILER_IS_GNUC AND
+            NOT ${CMAKE_C_COMPILER_VERSION} VERSION_LESS 4.6) OR
+        (CMAKE_C_COMPILER_ID STREQUAL "Intel" AND
+            NOT ${CMAKE_C_COMPILER_VERSION} VERSION_LESS 12.10) OR
+        (CMAKE_C_COMPILER_ID STREQUAL "PGI" AND
+            NOT ${CMAKE_C_COMPILER_VERSION} VERSION_LESS 14.3) OR
+        (CMAKE_C_COMPILER_ID STREQUAL "Clang" AND
+            NOT ${CMAKE_C_COMPILER_VERSION} VERSION_LESS 3.1))
+
+        set(${_VAR} 1)
+        message(STATUS "Checking for C99 compiler - available")
+
+    else()
+
+        message(STATUS "Checking for C99 compiler - unavailable")
+
+    endif()
+
+endmacro()
+
+# Sets the appropriate flag to enable C++11 support
+macro(enable_c99)
+    if(NOT CMAKE_C_COMPILER_ID STREQUAL "PGI")
+        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=c99")
+    endif(NOT CMAKE_C_COMPILER_ID STREQUAL "PGI")
+endmacro()
+
+#~---------------------------------------------------------------------------~-#
+# Formatting options for emacs and vim.
+# vim: set tabstop=4 shiftwidth=4 expandtab :
+#~---------------------------------------------------------------------------~-#
