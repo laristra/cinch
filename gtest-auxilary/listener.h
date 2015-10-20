8ef99f96e844d2f47b8cc8b2f99688badc8a73bf
Ben Bergen <ben.bergen@gmail.com>
2015-10-20 12:18:26 -0600

    Adding custom listener.

---
+++ gtest-auxilary/listener.h
@@ -0,0 +1,39 @@
+/*~--------------------------------------------------------------------------~*
+ * Copyright (c) 2015 Los Alamos National Security, LLC
+ * All rights reserved.
+ *~--------------------------------------------------------------------------~*/
+
+#ifndef cinch_listener_h
+#define cinch_listener_h
+
+#include <gtest/gtest.h>
+
+namespace cinch {
+
+class listener : public ::testing::EmptyTestEventListener
+{
+public:
+
+  virtual void OnTestStart(const ::testing::TestInfo& test_info) {
+    std::cout << "custom listner OnTestStart" << std::endl;
+  } // OnTestStart
+
+  virtual void OnTestPartResult(
+    const ::testing::TestPartResult& test_part_result) {
+    std::cout << "custom listner OnTestPartResult" << std::endl;
+  } // OnTestPartResult
+
+  virtual void OnTestEnd(const ::testing::TestInfo& test_info) {
+    std::cout << "custom listner OnTestEnd" << std::endl;
+  } // OnTestEnd
+
+}; // class listener
+
+} // namespace cinch
+
+#endif // cinch_listener_h
+
+/*~-------------------------------------------------------------------------~-*
+ * Formatting options for vim.
+ * vim: set tabstop=2 shiftwidth=2 expandtab :
+ *~-------------------------------------------------------------------------~-*/
