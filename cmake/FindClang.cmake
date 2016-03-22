1ee1f01933f791199dfe0a05f998d8d1b33a3ecc
Ben Bergen <bergen@lanl.gov>
2016-03-21 22:56:19 -0600

    Added Clang find package script

---
+++ cmake/FindClang.cmake
@@ -0,0 +1,84 @@
+#------------------------------------------------------------------------------#
+# Check for Clang.
+#------------------------------------------------------------------------------#
+
+include(FindPackageHandleStandardArgs)
+
+#------------------------------------------------------------------------------#
+# Look for executables (clang & clang++).
+#------------------------------------------------------------------------------#
+
+find_program(CLANG_EXEC NAMES "clang" PATH_SUFFIXES bin)
+find_program(CLANGXX_EXEC NAMES "clang++" PATH_SUFFIXES bin)
+
+mark_as_advanced(CLANG_EXEC CLANGXX_EXEC)
+
+#------------------------------------------------------------------------------#
+# Get version information.
+#------------------------------------------------------------------------------#
+
+execute_process(COMMAND ${CLANG_EXEC} --version
+    OUTPUT_VARIABLE CLANG_VERSION
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    ERROR_QUIET)
+
+# strip the extra lines from the output
+string(REGEX REPLACE "\n.*" "" CLANG_VERSION ${CLANG_VERSION})
+
+mark_as_advanced(CLANG_VERSION)
+
+#------------------------------------------------------------------------------#
+# Remove executable name and directory to try to get base directory name.
+#------------------------------------------------------------------------------#
+
+get_filename_component(_clang_base_dir "${CLANG_EXEC}" PATH)
+get_filename_component(_clang_base_dir "${_clang_base_dir}" PATH)
+
+#------------------------------------------------------------------------------#
+# Find clang include directory.
+#------------------------------------------------------------------------------#
+
+find_path(CLANG_INCLUDE_DIR clang/Basic/Version.h
+    HINTS ${_clang_base_dir}
+    PATH_SUFFIXES include
+)
+
+mark_as_advanced(CLANG_INCLUDE_DIR)
+
+#------------------------------------------------------------------------------#
+# Find clang libraries.
+#------------------------------------------------------------------------------#
+
+set(CLANG_LIBRARIES)
+
+foreach(_lib ${Clang_FIND_COMPONENTS})
+
+    find_library(_clang_${_lib} clang${_lib}
+        HINTS ${_clang_base_dir}
+        PATH_SUFFIXES lib lib64
+    )
+
+    if(_clang_${_lib})
+        list(APPEND CLANG_LIBRARIES ${_clang_${_lib}})
+    endif(_clang_${_lib})
+
+endforeach(_lib ${Clang_FIND_COMPONENTS})
+
+mark_as_advanced(CLANG_LIBRARIES)
+
+#------------------------------------------------------------------------------#
+# Standard argument handling.
+#------------------------------------------------------------------------------#
+
+find_package_handle_standard_args(clang
+    REQUIRED_VARS
+        CLANG_VERSION
+        CLANG_EXEC
+        CLANGXX_EXEC
+        CLANG_INCLUDE_DIR
+        CLANG_LIBRARIES
+)
+
+#------------------------------------------------------------------------------#
+# vim: set tabstop=4 shiftwidth=4 expandtab :
+#------------------------------------------------------------------------------#
