1f44d50643d3d402405fac047c03dfd271f8c877
Ben Bergen <bergen@lanl.gov>
2016-05-04 13:33:12 -0400

    Added root variable

--- cmake/FindKokkos.cmake
+++ cmake/FindKokkos.cmake
@@ -1,8 +1,11 @@
 #
+#  KOKKOS_ROOT         - Search path for Kokkos installation
 #  KOKKOS_INCLUDE_DIRS - where to find kokkos.h, etc
 #  KOKKOS_LIBRARIES    - List of libraries when using kokkos.
 #  KOKKOS_FOUND        - True if kokkos found.
 #
+
+set(KOKKOS_ROOT "" CACHE PATH "Root directory of Kokkos installation")
 
 find_path(KOKKOS_INCLUDE_DIR Kokkos_Core.hpp)
 find_library(KOKKOS_CORE_LIBRARY NAMES kokkoscore)
