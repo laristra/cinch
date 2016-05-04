7e2d2fff6d1b6cb26ca7674858ba235f95ce71db
Ben Bergen <bergen@lanl.gov>
2016-05-04 13:24:17 -0400

    Added Kokkos find script

---
+++ cmake/FindKokkos.cmake
@@ -0,0 +1,16 @@
+#
+#  KOKKOS_INCLUDE_DIRS - where to find kokkos.h, etc
+#  KOKKOS_LIBRARIES    - List of libraries when using kokkos.
+#  KOKKOS_FOUND        - True if kokkos found.
+#
+
+find_path(KOKKOS_INCLUDE_DIR Kokkos_Core.hpp)
+find_library(KOKKOS_CORE_LIBRARY NAMES kokkoscore)
+
+set(KOKKOS_LIBRARIES "${KOKKOS_CORE_LIBRARY}")
+set(KOKKOS_INCLUDE_DIRS "${KOKKOS_INCLUDE_DIR}")
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(KOKKOS DEFAULT_MSG KOKKOS_CORE_LIBRARY KOKKOS_INCLUDE_DIR)
+
+mark_as_advanced(KOKKOS_INCLUDE_DIR KOKKOS_CORE_LIBRARY)
