09ba3d83e45c075b971060299e22de6fe99fef58
Ben Bergen <bergen@lanl.gov>
2016-10-18 20:59:00 -0600

    Changed Find scripts to conform to CMake standard.

--- cmake/FindCereal.cmake
+++ cmake/FindCereal.cmake
@@ -10,21 +10,21 @@
 
 find_package(PkgConfig)
 
-pkg_check_modules(PC_CEREAL cereal)
+pkg_check_modules(PC_Cereal cereal)
 
 #------------------------------------------------------------------------------#
 # Options
 #------------------------------------------------------------------------------#
 
-set(CEREAL_ROOT "" CACHE PATH "Root directory of CEREAL installation")
+set(Cereal_ROOT "" CACHE PATH "Root directory of Cereal installation")
 
 #------------------------------------------------------------------------------#
 # Find the header file
 #------------------------------------------------------------------------------#
 
-find_path(CEREAL_INCLUDE_DIRS cereal.hpp
-    HINTS ENV CEREAL_ROOT
-    PATHS ${CEREAL_ROOT}
+find_path(Cereal_INCLUDE_DIRS cereal.hpp
+    HINTS ENV Cereal_ROOT
+    PATHS ${Cereal_ROOT}
     PATH_SUFFIXES include/cereal)
 
 #------------------------------------------------------------------------------#
@@ -33,9 +33,9 @@ find_path(CEREAL_INCLUDE_DIRS cereal.hpp
 
 include(FindPackageHandleStandardArgs)
 
-find_package_handle_standard_args(CEREAL REQUIRED_VARS CEREAL_INCLUDE_DIRS)
+find_package_handle_standard_args(Cereal REQUIRED_VARS Cereal_INCLUDE_DIRS)
 
-mark_as_advanced(CEREAL_INCLUDE_DIRS)
+mark_as_advanced(Cereal_INCLUDE_DIRS)
 
 #------------------------------------------------------------------------------#
 # Formatting options for emacs and vim.
