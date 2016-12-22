aec4066a120ecf55db4cc814df654d1219f598e4
Ben Bergen <bergen@lanl.gov>
2016-12-21 21:25:51 -0700

    Adding support for user initialization for gtest.

---
+++ gtest-auxiliary/gtest-init.h
@@ -0,0 +1,16 @@
+/*~--------------------------------------------------------------------------~*
+ * Copyright (c) 2015 Los Alamos National Security, LLC
+ * All rights reserved.
+ *~--------------------------------------------------------------------------~*/
+
+#ifndef gtest_init_h
+#define gtest_init_h
+
+void gtest_initialize(int argc, char ** argv) {}
+
+#endif // gtest_init_h
+
+/*~-------------------------------------------------------------------------~-*
+ * Formatting options for vim.
+ * vim: set tabstop=2 shiftwidth=2 expandtab :
+ *~-------------------------------------------------------------------------~-*/
--- gtest-auxiliary/gtest-legion.cc
+++ gtest-auxiliary/gtest-legion.cc
@@ -5,6 +5,18 @@
 
 #include <gtest/gtest.h>
 
+#define _UTIL_STRINGIFY(s) #s
+#define EXPAND_AND_STRINGIFY(s) _UTIL_STRINGIFY(s)
+
+#ifndef GTEST_INIT
+  #include "gtest-legion-init.h"
+#else
+  #include EXPAND_AND_STRINGIFY(GTEST_INIT)
+#endif
+
+#undef EXPAND_AND_STRINGIFY
+#undef _UTIL_STRINGIFY
+
 #include "listener.h"
 
 int main(int argc, char ** argv) {
@@ -12,8 +24,12 @@ int main(int argc, char ** argv) {
   // Initialize the GTest runtime
   ::testing::InitGoogleTest(&argc, argv);
 
+  // Get event listeners
   ::testing::TestEventListeners& listeners =
     ::testing::UnitTest::GetInstance()->listeners();
+
+  // Call the user-provided initialization function
+  gtest_init(argc, argv);
 
   // Adds a listener to the end.  Google Test takes the ownership.
   listeners.Append(new cinch::listener);
