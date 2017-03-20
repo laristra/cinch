bcc8020315cad31200d6e7dc6019132c573211f1
Ben Bergen <bergen@lanl.gov>
2017-03-20 10:38:43 -0600

    Fixing find script.

--- cmake/FindCaliper.cmake
+++ cmake/FindCaliper.cmake
@@ -3,31 +3,37 @@
 # All rights reserved.
 #------------------------------------------------------------------------------#
 
-# - Find flecsi
 # Find the native Caliper headers and libraries.
 #
 #  Caliper_INCLUDE_DIRS - where to find flecsi.h, etc.
 #  Caliper_LIBRARIES    - List of libraries when using flecsi.
 #  Caliper_FOUND        - True if flecsi found.
 
+find_package(PkgConfig)
+
+pkg_check_modules(PC_Caliper caliper)
+
 # Look for the header file.
-FIND_PATH(Caliper_INCLUDE_DIR
+FIND_PATH(Caliper_INCLUDE_DIRS
 	NAMES Annotation.h
+	HINTS ${PC_Caliper_INCLUDE_DIRS}
 	PATH_SUFFIXES caliper
 )
 
 # Look for the library.
-FIND_LIBRARY(Caliper_LIBRARY
+FIND_LIBRARY(Caliper_LIBRARIES
 	NAMES caliper libcaliper
+	HINTS ${PC_Caliper_LIBRARIES}
 )
 
 # handle the QUIETLY and REQUIRED arguments and set Caliper_FOUND to TRUE if 
 # all listed variables are TRUE
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Caliper Caliper_LIBRARY Caliper_INCLUDE_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Caliper Caliper_LIBRARIES
+	Caliper_INCLUDE_DIRS)
 
 # Copy the results to the output variables.
-SET(Caliper_LIBRARIES ${Caliper_LIBRARY})
-SET(Caliper_INCLUDE_DIRS ${Caliper_INCLUDE_DIR})
+SET(Caliper_LIBRARIES ${Caliper_LIBRARIES})
+SET(Caliper_INCLUDE_DIRS ${Caliper_INCLUDE_DIRS})
 
-MARK_AS_ADVANCED(Caliper_INCLUDE_DIR Caliper_LIBRARY)
+MARK_AS_ADVANCED(Caliper_INCLUDE_DIRS Caliper_LIBRARIES)
