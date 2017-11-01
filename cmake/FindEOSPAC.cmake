260509146d8b62de82deb6d9900c3a776296e00e
Marc Charest <charest@users.noreply.github.com>
2017-11-01 13:53:39 -0600

    Specify EOSPAC_LIBRARY_DIR instead of EOSPAC_LIBRARY on the command line

--- cmake/FindEOSPAC.cmake
+++ cmake/FindEOSPAC.cmake
@@ -17,9 +17,13 @@ endif()
 
 find_path(EOSPAC_INCLUDE_DIR eos_Interface.h
 	HINTS ${EOSPAC_ROOT}/include
-   PATHS ${EOSPAC_ROOT}/include)
+  PATHS ${EOSPAC_ROOT}/include)
 find_library(EOSPAC_LIBRARY NAMES eospac6
-	PATHS ${EOSPAC_ROOT}/lib ${EOSPAC_ROOT}/lib64)
+	PATHS
+    ${EOSPAC_ROOT}/lib
+    ${EOSPAC_ROOT}/lib64
+    ${EOSPAC_LIBRARY_DIR}
+)
 
 set(EOSPAC_LIBRARIES "${EOSPAC_LIBRARY}")
 set(EOSPAC_INCLUDE_DIRS "${EOSPAC_INCLUDE_DIR}")
@@ -27,4 +31,4 @@ set(EOSPAC_INCLUDE_DIRS "${EOSPAC_INCLUDE_DIR}")
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(EOSPAC DEFAULT_MSG EOSPAC_LIBRARY EOSPAC_INCLUDE_DIR)
 
-mark_as_advanced(EOSPAC_INCLUDE_DIR EOSPAC_LIBRARY)
+mark_as_advanced(EOSPAC_INCLUDE_DIR EOSPAC_LIBRARY EOSPAC_LIBRARY_DIR)
