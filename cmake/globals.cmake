b15376a58ef94fb11733e2166afb86bf1dce1b7d
charest <charest@users.noreply.github.com>
2018-11-16 16:40:58 +0000

    Revert "Changed CINCH_REQUIRED_CMAKE_VERSION to 3.11, since C++17 is not supp…"

--- cmake/globals.cmake
+++ cmake/globals.cmake
@@ -7,8 +7,8 @@
 # Require some version of cmake
 #------------------------------------------------------------------------------#
 
-# We are c++17, which is a cmake-3.8 feature
-set(CINCH_REQUIRED_CMAKE_VERSION 3.8)
+# We are using target_include_directories, which is a cmake-3.0 feature
+set(CINCH_REQUIRED_CMAKE_VERSION 3.0)
 
 cmake_minimum_required(VERSION ${CINCH_REQUIRED_CMAKE_VERSION})
 
