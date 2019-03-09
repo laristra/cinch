b35805fd2bde32c19946442de8503b89a3a10f24
Christoph Junghans <junghans@lanl.gov>
2019-03-08 19:36:09 -0700

    format.cmake: add support for nested projects

--- cmake/format.cmake
+++ cmake/format.cmake
@@ -5,18 +5,22 @@
 
 function(cinch_add_format)
 
+    if(NOT TARGET format)
+      add_custom_target(format)
+    endif()
     if(EXISTS ${PROJECT_SOURCE_DIR}/.clang-format)
       set(CLANG-FORMAT_BASE-VERSION "7.0.0")
       find_package(CLANG_FORMAT ${CLANG-FORMAT_BASE-VERSION})
       if (CLANG_FORMAT_FOUND)
         file(GLOB_RECURSE FORMAT_SOURCES SOURCES ${PROJECT_SOURCE_DIR}/*.cc ${PROJECT_SOURCE_DIR}/*.h)
-        add_custom_target(format COMMAND ${CLANG_FORMAT_EXECUTABLE} -style=file -i ${FORMAT_SOURCES})
+        add_custom_target(format-${PROJECT_NAME} COMMAND ${CLANG_FORMAT_EXECUTABLE} -style=file -i ${FORMAT_SOURCES})
       else()
-        add_custom_target(format COMMAND ${CMAKE_COMMAND} -E echo "No clang-format v${CLANG-FORMAT_BASE-VERSION} or newer found")
+        add_custom_target(format-${PROJECT_NAME} COMMAND ${CMAKE_COMMAND} -E echo "No clang-format v${CLANG-FORMAT_BASE-VERSION} or newer found")
       endif()
     else()
-      add_custom_target(format COMMAND ${CMAKE_COMMAND} -E echo "No ${PROJECT_SOURCE_DIR}/.clang-format found")
+      add_custom_target(format-${PROJECT_NAME} COMMAND ${CMAKE_COMMAND} -E echo "No ${PROJECT_SOURCE_DIR}/.clang-format found")
     endif()
+    add_dependencies(format format-${PROJECT_NAME})
 
 endfunction(cinch_add_format)
 
