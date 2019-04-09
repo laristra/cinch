816194e59a7af895add4afd6b84e6330756f1fd9
Ben Bergen <bergen@lanl.gov>
2019-04-09 15:21:50 -0700

    Formatting

--- cmake/boost.cmake
+++ cmake/boost.cmake
@@ -6,22 +6,17 @@ option(ENABLE_BOOST "Enable Boost" OFF)
 
 if(ENABLE_BOOST)
 
-    find_package(Boost REQUIRED 
-      program_options
-      atomic
-      filesystem
-      regex
-      system
-      QUIET)
+  find_package(Boost REQUIRED 
+    program_options
+    atomic
+    filesystem
+    regex
+    system
+    QUIET)
 
-    include_directories(${Boost_INCLUDE_DIRS})
-    link_directories(${Boost_LIBRARY_DIRS})
+  include_directories(${Boost_INCLUDE_DIRS})
+  link_directories(${Boost_LIBRARY_DIRS})
 
-    list(APPEND CINCH_RUNTIME_LIBRARIES ${Boost_LIBRARIES})
+  list(APPEND CINCH_RUNTIME_LIBRARIES ${Boost_LIBRARIES})
 
 endif()
-
-#------------------------------------------------------------------------------#
-# Formatting options for emacs and vim.
-# vim: set tabstop=4 shiftwidth=4 expandtab :
-#------------------------------------------------------------------------------#
