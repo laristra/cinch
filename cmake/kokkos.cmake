4565cd15afb83b5eb4dd064da1cc8a8ffc7320e2
Ben Bergen <bergen@lanl.gov>
2019-06-04 19:54:00 +0000

    Tweaked version requriements.

--- cmake/kokkos.cmake
+++ cmake/kokkos.cmake
@@ -6,6 +6,11 @@ option(ENABLE_KOKKOS "Enable Kokkos" OFF)
 
 if(ENABLE_KOKKOS)
 
+  if(NOT CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND
+    NOT ${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS 8)
+    message(FATAL_ERROR "Clang version 8 or greater required for Kokkos")
+  endif()
+
   find_package(Kokkos)
 
   include_directories(${Kokkos_INCLUDE_DIRS})
