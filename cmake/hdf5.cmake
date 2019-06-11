e46edaa26e0d3238906d026ca04c7bd97c5170ea
Ben Bergen <bergen@lanl.gov>
2019-06-11 20:39:28 +0000

    Fixed HDF5 logic.

--- cmake/hdf5.cmake
+++ cmake/hdf5.cmake
@@ -7,6 +7,11 @@ option(ENABLE_HDF5 "Enable HDF5" OFF)
 if(ENABLE_HDF5)
   find_package(HDF5 REQUIRED)
 
-  include_directories(${HDF5_INCLUDE_DIRS})
-  list(APPEND CINCH_RUNTIME_LIBRARIES ${HDF5_LIBRARIES})
+  include_directories(${HDF5_C_INCLUDE_DIR})
+
+  if(CMAKE_BUILD_TYPE MATCHES DEBUG)
+    list(APPEND CINCH_RUNTIME_LIBRARIES ${HDF5_hdf5_LIBRARY_DEBUG})
+  else()
+    list(APPEND CINCH_RUNTIME_LIBRARIES ${HDF5_hdf5_LIBRARY_RELEASE})
+  endif()
 endif()
